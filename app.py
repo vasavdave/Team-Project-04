4779a1a4067d33b3222f558c09b040f80727a8a8
MondragB <bryantmndrgn16@hotmail.com>
2021-11-06 13:48:33 -0400

    Form reworked, information passed between html and app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, request
 
+# Flask constructor
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template("index.html")
 
 
-@app.route('/data/', methods=['POST', 'GET'])
+@app.route('/data', methods=["GET", "POST"])
 def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html', form_data=form_data)
+    form_data = request.form
+    return render_template("data.html", form_data=form_data)
 
 
-app.run(host='localhost', port=5000)
+if __name__ == '__main__':
+    app.run()
